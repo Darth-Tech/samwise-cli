5bcb5c81a4680179fd32aa2e6021fa1013ba3233
thundersparkf <devagastya0@gmail.com>
2024-08-10 21:22:13 +0530

    adds viper config for output format, refactors code for allowing various output formats

--- cmd/checkForUpdates.go
+++ cmd/checkForUpdates.go
@@ -7,6 +7,8 @@ import (
 	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"github.com/thundersparkf/samwise/cmd/outputs"
 	"io/fs"
 	"log/slog"
 	"os"
@@ -31,19 +33,11 @@ JSON format: [{
 	Run: func(cmd *cobra.Command, args []string) {
 		slog.Debug("creating a CSV report...")
 		depth, rootDir, directoriesToIgnore := getParamsForCheckForUpdatesCMD(cmd.Flags())
-		slog.Debug("Params: ", strconv.Itoa(depth), rootDir, directoriesToIgnore)
+		slog.Debug("Params: ", slog.String("depth", strconv.Itoa(depth)), slog.String("rootDir", rootDir), slog.String("directoriesToIgnore", strings.Join(directoriesToIgnore, " ")))
 		rootDir = fixTrailingSlashForPath(rootDir)
 		var modules []map[string]string
 		var listWritten []string
-		slog.Debug("creating " + rootDir + "/module_dependency_report.csv file")
-		f, writer := CreateCSVReportFile(rootDir + "/module_dependency_report.csv")
-		slog.Debug("created " + rootDir + "/module_dependency_report.csv file")
-		defer f.Close()
-		_, err := writer.WriteString("repo_link,current_version,tag_list\n")
-		Check(err)
-		err = writer.Flush()
-		Check(err)
-		err = filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
+		err := filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
 			Check(err)
 			depthCountInCurrentPath := strings.Count(rootDir, string(os.PathSeparator))
 			if d.IsDir() && !slices.Contains(directoriesToIgnore, d.Name()) {
@@ -59,10 +53,9 @@ JSON format: [{
 					bar.Add(1)
 					slog.Debug(module["repo"])
 					if !slices.Contains(listWritten, module["repo"]) {
-						tagsList := processGitRepo(module["repo"], module["current_version"])
+						tagsList, _ := processGitRepo(module["repo"], module["current_version"])
 						if len(tagsList) > 0 {
-							writer.WriteString(module["repo"] + "," + module["current_version"] + "," + tagsList + "\n")
-							writer.Flush()
+							module["tags_available_for_update"] = tagsList
 							listWritten = append(listWritten, module["repo"])
 						}
 					}
@@ -71,6 +64,9 @@ JSON format: [{
 			return nil
 		})
 		Check(err)
+		outputFormat, err := checkOutputFormat(viper.GetString("output_format"))
+		Check(err)
+		generateReport(modules, outputFormat, rootDir)
 	},
 }
 
@@ -84,12 +80,19 @@ func getParamsForCheckForUpdatesCMD(flags *pflag.FlagSet) (int, string, []string
 	return depth, rootDir, directoriesToIgnore
 }
 
+func generateReport(data []map[string]string, outputFormat string, path string) {
+	if outputFormat == outputs.CSV {
+		CreateCSVReportFile(data, path)
+	}
+}
+
 func init() {
 	cobra.OnInitialize(initConfig)
 	checkForUpdatesCmd.Flags().IntP("depth", "d", 0, "Folder depth to search for modules in. Give -1 for a full directory extraction.")
 	checkForUpdatesCmd.Flags().StringP("path", "p", ".", "The path for directory containing terraform code to extract modules from.")
 	checkForUpdatesCmd.Flags().String("git-repo", "g", "Git Repository to check module dependencies on.")
-	checkForUpdatesCmd.Flags().StringArrayP("ignore", "i", []string{}, "Directories to ignore when searching for the One Ring(modules and their sources.")
+	checkForUpdatesCmd.Flags().StringArrayP("ignore", "i", []string{".git", ".idea"}, "Directories to ignore when searching for the One Ring(modules and their sources.")
+	checkForUpdatesCmd.Flags().StringP("output", "o", "csv", "Output format. Supports \"csv\" and \"json\". Default value is csv.")
 
 	rootCmd.AddCommand(checkForUpdatesCmd)
 
