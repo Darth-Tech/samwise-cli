afe5d1bf44588bd138638bf787bef98ff8f9750e
dev <devagastya0@gmail.com>
2024-08-30 08:27:42 +0530

    removes update flag in ci command

--- cmd/ci.go
+++ cmd/ci.go
@@ -95,5 +95,4 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	ciCmd.Flags().Bool("update", false, "Updates the modules in the files and pushes the changes into a branch.")
 }
